ce2ac01ba1cd340d5fa4c3b483aa4b829e1f007c
DjDhyogo <dyogo_leal@hotmail.com>
2025-06-04 23:33:01 -0300

    Feat Implementando rotas CRUD

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,103 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
+databese = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 async def root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get(
+    '/retornar_html', status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
+async def retornar_html():
+    return """
+<html>
+    <head>
+        <title>Olá Mundo!</title>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Olá Mundo!</h1>
+    </body>
+</html>
+    """
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(databese) + 1)
+    databese.append(user_with_id)
+    return user_with_id
+
+
+@app.get(
+    '/users/',
+    status_code=HTTPStatus.OK,
+    response_model=UserList,
+)
+def read_users():
+    return {'users': databese}
+
+
+@app.put(
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic,
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    if user_id < 1 or user_id > len(databese):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found!',
+        )
+    databese[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(databese):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found!',
+        )
+    return databese.pop(user_id - 1)
+
+
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def get_user(user_id: int):
+    if user_id < 1 or user_id > len(databese):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found!',
+        )
+    return databese[user_id - 1]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,127 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-client = TestClient(app)
-
-
-def test_root_deve_retornar_ola_mundo():
+def test_root_deve_retornar_ola_mundo(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_retornar_html_deve_retornar_html(client):
+    response = client.get('/retornar_html')
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+<html>
+    <head>
+        <title>Olá Mundo!</title>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Olá Mundo!</h1>
+    </body>
+</html>
+    """
+    )
+
+
+def test_create_user_deve_retornar_201(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Teste',
+            'email': 'dj@example.com',
+            'password': '123456',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'Teste',
+        'email': 'dj@example.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'Teste',
+                'email': 'dj@example.com',
+            }
+        ]
+    }
+
+
+def test_get_user(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'Teste',
+        'email': 'dj@example.com',
+    }
+
+
+def test_get_user_retornar_404(client):
+    response = client.get('/users/3')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found!'}
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'BOB',
+            'email': 'bob@example.com',
+            'password': '123456',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'BOB',
+        'email': 'bob@example.com',
+    }
+
+
+def test_update_user_deve_retornar_404(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'BOB',
+            'email': 'bob@example.com',
+            'password': '123456',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found!'}
+
+
+def test_deleteuser(client):
+    reponse = client.delete('/users/1')
+    assert reponse.status_code == HTTPStatus.OK
+    assert reponse.json() == {
+        'id': 1,
+        'username': 'BOB',
+        'email': 'bob@example.com',
+    }
+
+
+def test_delete_user_deve_retornar_404(client):
+    response = client.delete('/users/2')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found!'}
